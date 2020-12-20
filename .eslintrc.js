a83d8f72191ddd9438b47dd1dde6818b5f9b1b5a
Nikita Sviridenko <nikita.sviridenko@voypost.com>
2020-12-20 11:51:27 +0100

    Drop prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    // 'prettier',
+    // 'prettier/react',
+    // 'prettier/@typescript-eslint',
+    // 'plugin:prettier/recommended',
   ],
   plugins: ['react', '@typescript-eslint', 'jest'],
   env: {
@@ -34,12 +34,12 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
     'no-param-reassign': ['error', { props: false }],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-        trailingComma: 'all',
-      },
-    ],
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     endOfLine: 'auto',
+    //     trailingComma: 'all',
+    //   },
+    // ],
   },
 };
